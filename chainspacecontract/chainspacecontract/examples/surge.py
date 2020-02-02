395d748997bf37e4caefbd18dab42d8997ecf838
Shinjan Mitra <shinjan@cse.iitb.ac.in>
2020-02-02 10:18:56 +0530

    SREP creation contract complete

--- chainspacecontract/chainspacecontract/examples/surge.py
+++ chainspacecontract/chainspacecontract/examples/surge.py
@@ -65,7 +65,10 @@ def init():
 
     # return
     return {
-        'outputs': (dumps({'type' : 'InitToken', 'location':0}),dumps({'type' : 'InitToken', 'location':1}),dumps({'type' : 'InitToken', 'location':2})),
+        'outputs': (dumps({'type' : 'InitToken', 'location':0}),
+        dumps({'type' : 'InitToken', 'location':0}),
+        dumps({'type' : 'InitToken', 'location':1}),
+        dumps({'type' : 'InitToken', 'location':1})),
     }
 
 # ------------------------------------------------------------------
@@ -227,6 +230,153 @@ def cast_csc_vote_checker(inputs, reference_inputs, parameters, outputs, returns
     except Exception as e:
         print e
         return False
+    return True
+
+
+
+# ------------------------------------------------------------------
+# cast srep (shard representative) vote 
+# NOTE:
+#   - cast a vote to allow a client to act as shard representative
+#   - inputs must contain a valid VoteSlipToken
+#   - parameters contain a proof signed by the caster's private key
+# ------------------------------------------------------------------
+@contract.method('cast_srep_vote')
+def cast_srep_vote(inputs, reference_inputs, parameters, priv, granted_to_pub):
+
+    # vote_slip = inputs[0]
+    # proof = parameters[0]
+    granted_by_pub = loads(inputs[0])['pub']
+
+    vote_token = {
+        'type'          : 'SREPVoteToken', 
+        'granted_by'    : granted_by_pub,
+        'granted_to'    : granted_to_pub,
+        'location'      : loads(inputs[0])['location']
+    }
+
+    return {
+        'outputs': ( dumps(vote_token), inputs[0]),
+        'extra_parameters': (
+            generate_sig(priv),
+        )
+    }
+    
+# ------------------------------------------------------------------
+# check cast_srep_vote
+# ------------------------------------------------------------------
+@contract.checker('cast_srep_vote')
+def cast_srep_vote_checker(inputs, reference_inputs, parameters, outputs, returns, dependencies):
+    try:
+
+        # loads data
+        vote_slip = loads(inputs[0])
+        vote_token = loads(outputs[0])
+        new_vote_slip = loads(outputs[1])
+        
+        # check argument lengths
+        if len(inputs) != 1 or len(reference_inputs) != 0 or len(parameters)!=1 or len(outputs) != 2 or len(returns) != 0:
+            raise Exception("Invalid argument lengths")
+        # key validations
+        validate(vote_token, ['type','granted_by', 'granted_to','location'])
+        validate(new_vote_slip, ['type','pub','location'])
+        # type checks
+        check_type(vote_slip, 'VoteSlipToken')
+        check_type(vote_token, 'SREPVoteToken')
+        check_type(new_vote_slip, 'VoteSlipToken')
+        # equality checks
+        equate(vote_slip['pub'], new_vote_slip['pub'])
+        equate(vote_slip['location'], new_vote_slip['location'])
+        equate(vote_token['granted_by'], vote_slip['pub'])
+        equate(vote_token['location'], vote_slip['location'])        
+        # signature validation
+        validate_sig(parameters[0], vote_slip['pub'])
+        
+    except Exception as e:
+        print e
+        return False
+    return True
+
+
+# ------------------------------------------------------------------
+# create srep client
+# NOTE:
+#   - only 'inputs', 'reference_inputs' and 'parameters' are used to the framework
+#   - if there are more than 3 param, the checker has to be implemented by hand
+# ------------------------------------------------------------------
+@contract.method('create_srep_client')
+def create_srep_client(inputs, reference_inputs, parameters, priv):
+
+    pub = parameters[0]
+    # new client
+    srep_client = {
+        'type'           : 'SREPClient', 
+        'pub'            : pub, 
+        'location'       : loads(inputs[0])['location']
+    }
+    vote_slip = {
+        'type':'VoteSlipToken',
+        'pub':pub,
+        'location':loads(inputs[0])['location']
+    }
+    # return
+    return {
+        'outputs': ( dumps(srep_client), dumps(vote_slip)),
+        'extra_parameters': (
+            generate_sig(priv),
+        )
+    }
+
+# ------------------------------------------------------------------
+# check create_srep_client
+# ------------------------------------------------------------------
+@contract.checker('create_srep_client')
+def create_srep_client_checker(inputs, reference_inputs, parameters, outputs, returns, dependencies):
+    try:
+        REQUIRED_VOTES=2 # set to the number of CSCVoteTokens required to be accepted as a client
+
+        # loads data
+        srep_client = loads(outputs[0])
+        vote_slip = loads(outputs[1])
+        srep_vote_1 = loads(inputs[0])
+        
+        # check argument lengths
+        if len(reference_inputs) != 0 or len(parameters)!=2 or len(outputs) != 2 or len(returns) != 0:
+            raise Exception("Invalid argument lengths")
+        # key validations
+        validate(srep_client, ['type','pub','location'])
+        validate(vote_slip, ['type','pub','location'])
+        validate(srep_vote_1, ['type','granted_by', 'granted_to','location'])
+        
+        # type checks
+        check_type(srep_client, 'SREPClient')
+        check_type(vote_slip, 'VoteSlipToken')
+        check_type(srep_vote_1, 'SREPVoteToken')
+        # equality checks
+        equate(srep_client['pub'], parameters[0])
+        equate(srep_client['pub'], vote_slip['pub'])
+        equate(srep_client['location'], vote_slip['location'])
+        equate(srep_client['location'], srep_vote_1['location'])
+        
+        # signature validation
+        validate_sig(parameters[1], parameters[0])
+
+        # validate CSC votes if InitToken is not provided
+        voters = {}
+        for vote in inputs:
+            vote = loads(vote)
+            check_type(vote, 'SREPVoteToken')
+            equate(vote['granted_to'], parameters[0])
+            equate(vote['location'], srep_client['location'])
+            voters[str(vote['granted_by'])]= True
+        
+        if len(voters) < REQUIRED_VOTES:
+            raise Exception("Not enough voters")
+
+    except Exception as e:
+        print e
+        return False    
+    
     return True
 
 
